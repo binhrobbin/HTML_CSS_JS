ec3d9a85e3a9cb7f8a8a5d203352eef2748ba568
binhrobbin <binhrobbin.ace@gmail.com>
2023-12-16 11:24:10 +0700

    tạo mới

---
+++ Bài 6 Tổng quan js/6.demo2.js
@@ -0,0 +1,8 @@
+function chamchu(){
+    alert('mày vừa click tao')
+}
+var moi= document.getElementById('demo');
+moi.addEventListener('click', chamvaochu)
+function chamvaochu(){
+    alert('xin chào')
+}
